c795919eecdc972c6bad5c202f2429d18da22371
Juan Gonzales <jgonzalesbazan@gmail.com>
2019-01-30 14:25:47 -0500

    Fin de la sección 16

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
+// Rutas hijas
 import { PAGES_ROUTES } from './pages.routes';
 // temporal
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
@@ -16,12 +17,17 @@ import { PipesModule } from '../pipes/pipes.module';
 
 // ng2-charts
 import { ChartsModule } from 'ng2-charts';
+
+// Pages
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { ModalUploadComponent } from '../components/modal-upload/modal-upload.component';
+import { HospitalesComponent } from './hospitales/hospitales.component';
+import { MedicosComponent } from './medicos/medicos.component';
+import { MedicoComponent } from './medicos/medico.component';
 
 
 @NgModule({
@@ -37,7 +43,10 @@ import { ModalUploadComponent } from '../components/modal-upload/modal-upload.co
         RxjsComponent,
         ProfileComponent,
         UsuariosComponent,
-        ModalUploadComponent
+        ModalUploadComponent,
+        HospitalesComponent,
+        MedicosComponent,
+        MedicoComponent
     ],
     imports: [
         CommonModule,
---
+++ src/app/services/medico/medico.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../config/config';
+import { map } from 'rxjs/operators';
+import { UsuarioService } from '../usuario/usuario.service';
+import { Medico } from '../../models/medico.model';
+import swal from 'sweetalert';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MedicoService {
+
+  totalMedicos: number = 0;
+  constructor(public http: HttpClient,
+              public _usuarioService: UsuarioService) { }
+
+  cargarMedicos() {
+    const url = URL_SERVICIOS + '/medico';
+    return this.http.get(url).pipe(
+      map( (resp: any) => {
+        this.totalMedicos = resp.total;
+        return resp.medicos;
+      })
+    );
+  }
+
+  cargarMedico(id: string) {
+   const url = URL_SERVICIOS + '/medico/' + id;
+   return this.http.get(url).pipe(
+     map((resp: any) => {
+        return resp.medico;
+     })
+   );
+  }
+
+  buscarMedicos(termino: string) {
+    const url = URL_SERVICIOS + '/busqueda/coleccion/medicos/' + termino;
+
+    return this.http.get(url)
+            .pipe(
+              map( (resp: any) => {
+                return resp.medicos;
+              })
+            );
+  }
+
+  borrarMedico(id: string) {
+    let url = URL_SERVICIOS + '/medico/' + id ;
+    url += '?token=' + this._usuarioService.token;
+
+    return this.http.delete(url).pipe(
+      map((resp) => {
+        swal('Médico borrado', 'Médico borrado correctamente', 'success');
+        return resp;
+      })
+    );
+  }
+
+  guardarMedico(medico: Medico) {
+    let url = URL_SERVICIOS + '/medico';
+
+    if (medico._id) {
+      // actualizando
+      url += '/' + medico._id;
+      url += '?token=' + this._usuarioService.token;
+
+      return this.http.put(url, medico).pipe(
+        map(((resp: any) => {
+          swal('Médico Actualizado', medico.nombre, 'success');
+              return  resp.medico;
+        }))
+      );
+    } else {
+      // creando
+          url += '?token=' + this._usuarioService.token;
+
+          return this.http.post(url, medico).pipe(
+            map((resp: any) => {
+              swal('Médico Creado', medico.nombre, 'success');
+              return  resp.medico;
+            })
+          );
+    }
+  }
+}
